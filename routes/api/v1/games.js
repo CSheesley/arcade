f807daa36401a29bbe40b1ba08e14eea063fb634
Corey Sheesley <corey.sheesley@gmail.com>
2019-06-17 20:13:13 -0600

    Adds update route

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -51,5 +51,31 @@ router.post("/", function(req, res, next) {
 });
 
 
+// Update existing game
+router.put("/:id", function(req, res, next) {
+  Game.update({
+    title: req.body.title,
+    price: req.body.price,
+    releaseYear: req.body.releaseYear,
+    active: req.body.active
+  },
+  {
+    returning: true,
+    where:{
+      id: parseInt(req.params.id)
+    }
+  }
+)
+    .then(game => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(201).send(JSON.stringify(game));
+  })
+    .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({ error });
+  });
+});
+
+
 //keep at bottom of file
 module.exports = router
